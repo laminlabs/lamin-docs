d955b635266f5194afb3cf3b2b390d4da4405d93
Alex Wolf <f.alexander.wolf@gmail.com>
2022-10-23 14:42:07 +0200

    :green_heart: Fix CI

---
+++ docs/conf.py
@@ -0,0 +1,15 @@
+# isort:skip_file
+import sys
+from pathlib import Path
+
+HERE = Path(__file__).parent
+sys.path[:0] = [str(HERE), str(HERE.parent)]
+
+from lamin_sphinx import *  # noqa
+from lamin_sphinx import authors, html_theme_options, html_context  # noqa
+
+project = "Lamin Docs"
+html_title = f"{project}"
+html_context["github_repo"] = "lamin-docs"  # noqa
+# We're actually using this for the link behind the brand of the page!
+html_theme_options["logo"] = {"link": "../docs"}  # noqa
