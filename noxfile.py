4aab7a97f9b7be24fc73d3f849bb7b009e2ea53b
Alex Wolf <f.alexander.wolf@gmail.com>
2023-02-27 22:17:37 +0100

    📝 Add redun use case (#8)
    * :memo: Add redun use case
    
    * :memo: Fix name
    
    * :pencil2: Fix typo
    
    * :memo: Fix all paths
    
    * :green_heart: Fix

--- noxfile.py
+++ noxfile.py
@@ -23,6 +23,17 @@ README_SECTION = """\
 ```
 """
 
+USE_CASES = """
+```{toctree}
+:maxdepth: 1
+:hidden:
+:caption: Use cases
+
+redun-get-started
+redun-run-workflow
+```
+"""
+
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def build(session):
@@ -31,13 +42,19 @@ def build(session):
 
     import lamindb as ln
 
+    # LaminDB
+
     dobject = ln.select(ln.DObject, name="lamindb_docs").one()
-    shutil.unpack_archive(dobject.load(), "lamindb_docs")
+    shutil.unpack_archive(dobject.load(), "tmp")
+    Path("tmp/docs").rename("lamindb_docs")
     Path("lamindb_docs/guide").rename("docs/guide")
     Path("lamindb_docs/faq").rename("docs/faq")
 
+    # Setup / Lamin
+
     dobject = ln.select(ln.DObject, name="lndb_docs").one()
-    shutil.unpack_archive(dobject.load(), "lndb_docs")
+    shutil.unpack_archive(dobject.load(), "tmp")
+    Path("tmp/docs").rename("lndb_docs")
     Path("lndb_docs").rename("docs/setup")
 
     with open("docs/setup/index.md") as f:
@@ -46,10 +63,27 @@ def build(session):
         content = content.replace(README_SECTION, "# Setup")
         f.write(content)
 
-    # changes working directory
-    # execute_notebooks(Path("./docs/cli.ipynb").resolve(), write=True)
-    # os.chdir("..")
+    # Use cases
 
+    dobject = ln.select(ln.DObject, name="redun_lamin_fasta_docs").one()
+    shutil.unpack_archive(dobject.load(), "tmp")
+    Path("tmp/docs").rename("redun_lamin_fasta_docs")
+    Path("redun_lamin_fasta_docs/guide/1-get-started.ipynb").rename(
+        "docs/guide/redun-get-started.ipynb"
+    )
+    Path("redun_lamin_fasta_docs/guide/2-run-workflow.ipynb").rename(
+        "docs/guide/redun-run-workflow.ipynb"
+    )
+
+    with open("docs/guide/index.md") as f:
+        content = f.read()
+    with open("docs/guide/index.md", "w") as f:
+        content += USE_CASES
+        f.write(content)
+
+    # Build docs
+
+    # init an instance so that docs can be built
     lamin.init(storage="mydata")
     session.install("lamindb")
     build_docs(session)
