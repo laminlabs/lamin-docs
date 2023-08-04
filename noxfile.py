9dd45f7e8ee701ad3bbe1b439fa8e78bc461d056
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-04 22:47:04 +0200

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -121,4 +121,4 @@ def docs(session):
     else:
         session.install(f"{prefix}/lndocs")
     # do not simply add instance creation here
-    session.run("lndocs", "--strip-prefix", "--fix-images")
+    session.run("lndocs", "--strip-prefix", "--error-on-index")
