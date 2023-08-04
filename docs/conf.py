3d97b7f1df0518c7ec761ec236d758eeb58ed792
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-04 20:24:05 +0200

    📝 Fix docs link behind Lamin Docs

--- docs/conf.py
+++ docs/conf.py
@@ -6,13 +6,13 @@ HERE = Path(__file__).parent
 sys.path[:0] = [str(HERE), str(HERE.parent)]
 
 from lamin_sphinx import *  # noqa
-from lamin_sphinx import authors, html_theme_options, html_context  # noqa
+from lamin_sphinx import html_theme_options, html_context  # noqa
 
 project = "Lamin Docs"
 html_title = f"{project}"
 html_context["github_repo"] = "lamin-docs"  # noqa
 
-ogp_site_url = "https://lamin.ai/docs/guide/"
+ogp_site_url = "https://lamin.ai/docs"
 ogp_site_name = project
 
 # We're actually using this for the link behind the brand of the page!
