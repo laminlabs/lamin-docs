b33ec97c1d061b6eebec276b61c7afd3f70bb5f4
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-08-09 12:26:51 +0200

    🧪 Test

--- noxfile.py
+++ noxfile.py
@@ -105,11 +105,12 @@ def pull_artifacts(session):
     pull_from_s3_and_unpack("redun_lamin_fasta_docs.zip")
     Path("redun_lamin_fasta_docs/guide/1-redun.ipynb").rename("docs/redun.ipynb")
     # usescases
+    Path("docs/usecases").mkdir()
     pull_from_s3_and_unpack("lamin_usecases_docs.zip")
     for path in Path("lamin_usecases_docs/").glob("*"):
         if path.name == "index.md":
             continue
-        path.rename(Path("docs") / path.name)
+        path.rename(Path("docs/usecases") / path.name)
 
     with open("docs/guide.md") as f:
         content = f.read()
