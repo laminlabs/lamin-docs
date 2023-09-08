b789e70bd390c4a50958b1f9d3511520e782d734
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2023-09-08 16:24:08 -0400

    ✨ Added cellxgene-census (#59)
    * ✨ Added cellxgene-census
    
    * 📝 Fix title

--- noxfile.py
+++ noxfile.py
@@ -44,6 +44,7 @@ USECASES_TEXT = """
 ../by-registry
 ../by-datatype
 pipelines
+census
 ```
 """
 
@@ -135,6 +136,19 @@ def pull_artifacts(session):
     Path("nextflow_lamin_usecases_docs/guide/bulk_rna_seq.ipynb").rename(
         "docs/nextflow.ipynb"
     )
+
+    # cellxgene-census
+    pull_from_s3_and_unpack("cellxgene_census_lamin_docs.zip")
+    for path in Path("cellxgene_census_lamin_docs/").glob("*"):
+        if path.name == "guide.md":
+            with open(path.as_posix()) as f:
+                content = f.read()
+            with open(path.as_posix(), "w") as f:
+                content = content.replace("# Guide", "# Atlases")
+                f.write(content)
+            sync_path(path, Path("docs") / "census.md")
+        elif path.name.endswith(".ipynb"):
+            sync_path(path, Path("docs") / path.name)
 
     # use-cases
     pull_from_s3_and_unpack("lamin_usecases_docs.zip")
