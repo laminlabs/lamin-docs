e681f4e72a923831241ac04711b1fc6454e9931c
Alex Wolf <f.alexander.wolf@gmail.com>
2024-09-07 18:40:36 +0200

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -215,7 +215,7 @@ def pull_artifacts(session):
 
 @nox.session
 def docs(session):
-    run("pip install --no-deps git+https://github.com/laminlabs/lnschema-core")
+    run(session, "pip install --no-deps git+https://github.com/laminlabs/lnschema-core")
     # run(session, "pip install git+https://github.com/laminlabs/bionty")
     run(session, "pip install --no-deps git+https://github.com/laminlabs/wetlab")
     run(
