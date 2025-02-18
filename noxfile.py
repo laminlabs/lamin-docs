f27452b076210e6abfa4ad7988ee2d7c01e3f94c
Alex Wolf <f.alexander.wolf@gmail.com>
2025-02-18 23:55:42 +0100

    📝 Add examples to announcement

--- noxfile.py
+++ noxfile.py
@@ -281,7 +281,7 @@ def init(session):
 @nox.session
 def docs(session):
     process = subprocess.run(  # noqa S602
-        "lndocs --strip-prefix --error-on-index", shell=True
+        "lndocs --strict --strip-prefix --error-on-index", shell=True
     )
     if process.returncode != 0:
         # rerun without strict option so see all warnings
