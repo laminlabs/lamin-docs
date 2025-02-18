5d8f4af8829aae5ea84c17c76a0aa368ce1f814e
Alex Wolf <f.alexander.wolf@gmail.com>
2025-02-18 15:37:55 +0100

    📝 Add perturbation notebook back

--- noxfile.py
+++ noxfile.py
@@ -63,6 +63,7 @@ multimodal
 
 BY_DATATYPE = """
 multimodal
+perturbation
 ```
 """
 
@@ -218,11 +219,11 @@ def pull_artifacts(session):
     replace_content("docs/by-datatype.md", {BY_DATATYPE_ORIG: BY_DATATYPE})
 
     # wetlab (must be after use-cases)
-    # pull_from_s3_and_unpack("wetlab.zip")
-    # sync_path(
-    #     Path("wetlab/guide/pert-curator.ipynb"),
-    #     Path("docs/perturbation.ipynb"),
-    # )
+    pull_from_s3_and_unpack("wetlab.zip")
+    sync_path(
+        Path("wetlab/guide/pert-curator.ipynb"),
+        Path("docs/perturbation.ipynb"),
+    )
 
     # amend toctree
     with open("docs/guide.md") as f:
