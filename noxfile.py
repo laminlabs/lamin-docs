093c71baebd0019ccd0f613c81a64f8f577d6a2f
Alex Wolf <f.alexander.wolf@gmail.com>
2025-09-27 19:34:46 +0200

    💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -143,7 +143,9 @@ README1_ORIG = """</details>
 
 **Highlights.**"""
 
-README1_REPLACE = """```{dropdown} LaminDB Highlights"""
+README1_REPLACE = """```
+
+```{dropdown} LaminDB Highlights"""
 
 README2_ORIG = """
 LaminDB can be connected to LaminHub to serve as a [LIMS](https://en.wikipedia.org/wiki/Laboratory_information_management_system) for wetlab scientists, closing the drylab-wetlab feedback loop: [lamin.ai](https://lamin.ai)
