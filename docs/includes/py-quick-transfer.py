36d1f2dffda25780c141b4ccaa9a0144dd009378
Alex Wolf <f.alexander.wolf@gmail.com>
2025-11-12 21:01:33 +0100

    ♻️ .using() to .connect() (#366)

--- docs/includes/py-quick-transfer.py
+++ docs/includes/py-quick-transfer.py
@@ -2,17 +2,14 @@ import lamindb as ln
 
 # Access inputs -------------------------------------------
 
-ln.track()  # track your run of a notebook or script
-artifact = ln.Artifact.using("laminlabs/cellxgene").get(
-    "7dVluLROpalzEh8m"
-)  # query the artifact https://lamin.ai/laminlabs/cellxgene/artifact/7dVluLROpalzEh8m
-adata = artifact.load()[
-    :, :100
-]  # load into memory or sync to cache: filepath = artifact.cache()
+ln.track()
+cellxgene_artifacts = ln.Artifact.connect("laminlabs/cellxgene")
+artifact = cellxgene_artifacts.get("7dVluLROpalzEh8m")
+adata = artifact.load()[:, :100]
 
 # Your transformation -------------------------------------
 
-import scanpy as sc  # find marker genes with Scanpy
+import scanpy as sc
 
 sc.pp.normalize_total(adata)
 sc.pp.log1p(adata)
@@ -20,7 +17,5 @@ sc.tl.rank_genes_groups(adata, groupby="cell_type")
 
 # Save outputs --------------------------------------------
 
-ln.Artifact.from_anndata(
-    adata, key="my-datasets/my-result.h5ad"
-).save()  # save versioned output
-ln.finish()  # finish the run, save source code & run report
+ln.Artifact.from_anndata(adata, key="my-datasets/my-result.h5ad").save()
+ln.finish()
