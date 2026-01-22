d405afa79550d98980a384bcb79aa9ae3d3efe06
Alex Wolf <f.alexander.wolf@gmail.com>
2026-01-22 15:32:10 +0100

    ♻️ Refine hub highlights

--- docs/includes/specs-laminhub.md
+++
@@ -1,7 +0,0 @@
-It provides a GUI for LaminDB and makes it easy to manage many LaminDB instances including hosting, access management, and soon, audit logs.
-
-Through the open-source LaminDB, developers retain full access to their data with zero lock-in danger. Instead of depending on rate-limited REST APIs, developers build directly with Postgres & diverse open storage formats on object stores.
-
-You can give it a try by exploring public omics data collections at [lamin.ai/explore](https://lamin.ai/explore). It's free and no account is required.
-
-LaminHub is a SaaS product. For private data & commercial usage, see: [lamin.ai/pricing](https://lamin.ai/pricing).
--- noxfile.py
+++ noxfile.py
@@ -66,9 +66,18 @@ LaminHub is a data collaboration hub built on LaminDB similar to how GitHub is b
 
 :::{dropdown} Hub highlights
 
-```{include} includes/specs-laminhub.md
+- a GUI
+- permission management
+- organization & team management
+- hosting
+- audit logs
+- authentication & SSO
 
-```
+Through the open-source LaminDB, developers retain full access to their data with zero lock-in danger. Instead of depending on rate-limited REST APIs, developers build directly with Postgres & diverse open storage formats on object stores.
+
+Give it a try by exploring public omics datasets at [lamin.ai/explore](https://lamin.ai/explore). It's free and no account is required.
+
+LaminHub is a SaaS product. For private data & commercial usage, see: [lamin.ai/pricing](https://lamin.ai/pricing).
 
 :::
 
