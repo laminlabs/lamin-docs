750b224064f286b3cea68d59df83385561a11b11
Alex Wolf <f.alexander.wolf@gmail.com>
2026-04-16 11:53:25 +0200

    💚 Actually

--- noxfile.py
+++ noxfile.py
@@ -275,9 +275,11 @@ def pull_artifacts(session):
         assert README0_ORIG in content  # noqa: S101
         assert README1_ORIG in content  # noqa: S101
         assert README2_ORIG in content  # noqa: S101
+        assert README3_ORIG in content  # noqa: S101
         content = content.replace(README0_ORIG, README0_REPLACE)
         content = content.replace(README1_ORIG, README1_REPLACE)
         content = content.replace(README2_ORIG, README2_REPLACE)
+        content = content.replace(README3_ORIG, README3_REPLACE)
         content = convert_markdown_python_to_tabbed(content)
         f.write(content)
 
