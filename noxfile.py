cff05c62854df2e7e3de33dafc64163b1bff87c2
Alex Wolf <f.alexander.wolf@gmail.com>
2026-07-07 12:40:40 +0200

    ♻️ Pull mlops content from source repo

--- noxfile.py
+++ noxfile.py
@@ -247,11 +247,13 @@ def pull_artifacts(session):
 
     # mlops
     pull_from_s3_and_unpack("lamin-mlops.zip")
-    Path("lamin-mlops/mnist.ipynb").rename("docs/mnist.ipynb")
-    Path("lamin-mlops/wandb.ipynb").rename("docs/wandb.ipynb")
-    Path("lamin-mlops/mlflow.ipynb").rename("docs/mlflow.ipynb")
-    Path("lamin-mlops/autoencoder.py").rename("docs/autoencoder.py")
-    Path("lamin-mlops/croissant.ipynb").rename("docs/croissant.ipynb")
+    for path in Path("lamin-mlops/").glob("**/*"):
+        if path.name in {"index.md", "conf.py"}:
+            continue
+        if path.is_dir():
+            sync(path, Path("docs") / path.name, "sync", create=True, ctime=True)
+        else:
+            sync_path(path, Path("docs") / path.name)
 
     # cellxgene-lamin
     pull_from_s3_and_unpack("cellxgene-lamin.zip")
